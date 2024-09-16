49321b6ffec2f5b810c037d3b9300539bb3b81b3
Rocowoco <rocowoco@yahoo.com>
2024-09-16 15:23:38 -0700

    model for user (pillow)

--- apps/users/models.py
+++ apps/users/models.py
@@ -2,8 +2,23 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
-    bio = models.TextField(null=True, blank=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='customuser_groups',
+        blank=True,
+        help_text='The groups this user belongs to.',
+        verbose_name='groups'
+    )
+    
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='customuser_user_permissions',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions'
+    )
 
     def __str__(self):
         return self.username
